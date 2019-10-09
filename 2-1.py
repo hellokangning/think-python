5e8e80a1f32bea0746320495035931742bbc8e86
Guoqing Geng <hellokangning@hotmail.com>
2019-10-09 21:44:53 +0800

    2

---
+++ 2-1.py
@@ -0,0 +1,4 @@
+zipcode = 02132 
+print(zipcode) # 1114
+
+zipcode = 02492 # invalid token
---
+++ 2-2.py
@@ -0,0 +1,8 @@
+5
+x = 5
+x + 1
+
+print("===========")
+print(5)
+print(x = 5) # Invalid syntax
+print(x + 1)
